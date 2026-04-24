41d26a4067d716c738732df5d4397182d86fc3ea
1Kanra <notfromouterspace.turtle@gmail.com>
2026-04-24 11:30:05 +0800

    fixed?

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+function setStyle(num) {
+  document.body.className = "style-" + num;
+}
+
 fetch('links.json')
   .then(res => res.json())
   .then(data => {
+
     const container = document.getElementById('links');
 
+    // STYLE 1 (default links)
     data.links.forEach(link => {
       const a = document.createElement('a');
       a.href = link.url;
@@ -11,32 +17,3 @@ fetch('links.json')
       container.appendChild(a);
     });
   });
-
-function setStyle(num) {
-  document.body.className = "style-" + num;
-}
-
-data.links.forEach((link, i) => {
-  const item = document.createElement('div');
-  item.className = "hex-item";
-
-  item.innerHTML = `
-    <div class="hex">${link.name[0]}</div>
-    <div class="label">${link.name}</div>
-  `;
-
-  item.onclick = () => window.open(link.url, "_blank");
-
-  container.appendChild(item);
-});
-
-const center = document.createElement('div');
-center.id = "centerDisplay";
-document.body.appendChild(center);
-
-document.addEventListener("mouseover", (e) => {
-  if (e.target.closest(".hex-item")) {
-    const text = e.target.closest(".hex-item").innerText;
-    center.textContent = text;
-  }
-});
