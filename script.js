4668d5060b73d3eaf7afe72c77e0704005453f5f
1Kanra <notfromouterspace.turtle@gmail.com>
2026-04-24 11:27:01 +0800

    testing styles

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ fetch('links.json')
       container.appendChild(a);
     });
   });
+
+function setStyle(num) {
+  document.body.className = "style-" + num;
+}
+
+data.links.forEach((link, i) => {
+  const item = document.createElement('div');
+  item.className = "hex-item";
+
+  item.innerHTML = `
+    <div class="hex">${link.name[0]}</div>
+    <div class="label">${link.name}</div>
+  `;
+
+  item.onclick = () => window.open(link.url, "_blank");
+
+  container.appendChild(item);
+});
+
+const center = document.createElement('div');
+center.id = "centerDisplay";
+document.body.appendChild(center);
+
+document.addEventListener("mouseover", (e) => {
+  if (e.target.closest(".hex-item")) {
+    const text = e.target.closest(".hex-item").innerText;
+    center.textContent = text;
+  }
+});
